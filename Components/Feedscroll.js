8456c59e8b6deffb64b128b6b59beac9ebd60cee
Venkatesh Kashyap <89914566+Venkatesh360@users.noreply.github.com>
2023-06-16 11:43:07 +0530

    Update Feedscroll.js
    removed semicolon

--- Components/Feedscroll.js
+++ Components/Feedscroll.js
@@ -5,7 +5,7 @@ const Feedscroll = ({data}) => {
   return (
     <div className=" font-montserrat sm:grid md:grid-cols-3 lg:grid-cols-3
      2xl:grid-cols-4" >
-       {data.map((results) => <FeedCard result={results} key={results.id} />)};
+       {data.map((results) => <FeedCard result={results} key={results.id} />)}
     </div>
   )
 }
